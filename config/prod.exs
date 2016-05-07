ba5cd3c7906fc6ff042c7dece116b02645f5eab5
Romain Champourlier <pro@rchampourlier.com>
2016-05-07 23:00:59 +0200

    Changes for Heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :idea_zone, IdeaZone.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: System.get_env("HOST"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,9 +58,12 @@ config :logger, level: :info
 #     config :idea_zone, IdeaZone.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure your database
+config :idea_zone, IdeaZone.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20,
+  ssl: true
 
 # Custom config
 config :idea_zone,
