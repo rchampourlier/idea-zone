e605d924e05112f8040d4f4d6a52c175c1ea0eb6
Romain Champourlier <pro@rchampourlier.com>
2016-05-07 23:15:42 +0200

    Switching to HTTP for SGC
    (cherry picked from commit 4ba0b98b20816f32e1a62ccd5ccd12402912ce3c)

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :idea_zone, IdeaZone.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: System.get_env("HOST"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: System.get_env("HOST")],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
